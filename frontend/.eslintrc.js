a20b6812c81428f837620fb90628ecfcfb4ac301
dev-blc <genesis.dev.blc@gmail.com>
2025-12-19 21:06:09 +0530

    fixes linter

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,21 +1,4 @@
 module.exports = {
-  extends: [],
-  plugins: ['react-hooks'],
-  rules: {
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn'
-  },
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true
-    }
-  },
-  env: {
-    browser: true,
-    es2020: true,
-    node: true
-  }
+  root: true,
+  extends: ["react-app", "react-app/jest"],
 };
-
